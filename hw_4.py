7faaacecccc486c46c77e2973648627504ba53d1
albi-ll <albinamairambek1@gmail.com>
2025-12-17 20:00:53 +0600

    hw_4

--- hw_4.py
+++ hw_4.py
@@ -3,6 +3,7 @@ class Contact:
         self.name = name
         self.phone_number = phone_number
 
+    @staticmethod
     def  validate_phone_number(phone_number):
         if len(phone_number) == 10:
             return True
@@ -14,18 +15,17 @@ class Contact:
 class ContactList:
     all_contacts = []
 
-    def add_contact(name, phone_number):
-        Contact.validate_phone_number(phone_number)
+    @classmethod
+    def add_contact(cls, name, phone_number):
+        if Contact.validate_phone_number(phone_number):
+            cls.all_contacts.append(Contact(name, phone_number))
+        else:
+            print("Ошибка")
 
 
 ContactList.add_contact("Вася Пупкин", "0700100200")
 ContactList.add_contact("Виктор Цой", "0500123456")
+ContactList.add_contact("John Doe", "5551234")
 
 for contact in ContactList.all_contacts:
-    if Contact.validate_phone_number(contact.phone_number):
-        ContactList.add_contact("Вася Пупкин", "0700100200")
-        print(contact.name, contact.phone)
-    elif ContactList.add_contact("John Doe", "5551234"):
-        print("Ошибка")
-
-print(ContactList.all_contacts)
+    print(contact.name, contact.phone_number)
